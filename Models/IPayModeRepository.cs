e974d6e54fe84854463cc0f6d396b4575347d60e
erick.mauricio30@hotmail.com <erick.mauricio30@hotmail.com>
2024-04-20 06:49:03 -0500

    CategoríaModelo

--- Models/IPayModeRepository.cs
+++ Models/IPayModeRepository.cs
@@ -1,4 +1,10 @@
-﻿namespace Supermarket_mvp.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
 {
     internal interface IPayModeRepository
     {
